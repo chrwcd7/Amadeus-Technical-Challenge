7e140b3b35d330e1eaa0d303669ccdee0ff5540b
Chris <chrispapoutsakis@gmail.com>
2019-03-07 10:49:02 +0100

    added assumption 4 and respective code

--- map_string_replace.js
+++ map_string_replace.js
@@ -1,4 +1,4 @@
-if (process.argv.length < 3) {                                                          // Check if an input file is provided
+if (process.argv.length < 3) {                                                              // Check if an input file is provided
     console.log('Usage: node ' + process.argv[1] + ' FILENAME');
     process.exit(1);
 }
@@ -8,7 +8,7 @@ const readline = require('readline');
 const filename = process.argv[2];
 
 const myMap = new Map();
-const regex = /\${([^}]+)}/;                                                            // Use a regular expression to match the pattern ${key}
+const regex = /\${([^}]+)}/;                                                                // Use a regular expression to match the pattern ${key}
 
 const lineReader = readline.createInterface({
     input: fs.createReadStream(filename)
@@ -16,9 +16,10 @@ const lineReader = readline.createInterface({
 
 let lineIndex = 1;
 let pair;
+let mapIsPure;                                                                              // Initially we assume the map is pure (meaning no keys are referenced in the values)
 
 lineReader
-    .on('line', line => {                                                               // Read the file line by line and construct the map
+.on('line', line => {                                                                       // Read the file line by line and construct the map
         pair = line.split(' ');
         if (!pair[0]) {
             throw new Error("Input map is of incorrect format: Key is not provided on line " + lineIndex);
@@ -32,12 +33,19 @@ lineReader
     .on('close', () => {
         console.log('\nOutput map');
 
-        for (let [key, value] of myMap.entries()) {                                     // Iterate the map
-        
-            if (regex.test(value)) {                                                    // Test the value for a match
-                myMap.set(key, value.replace(regex, myMap.get(regex.exec(value)[1])));  // Replace the value of the key
+        do {
+            mapIsPure = 1;
+            for (let [key, value] of myMap.entries()) {                                     // Iterate the map
+                
+                if (regex.test(value)) {                                                    // Test the value for a match
+                    mapIsPure = 0;
+                    myMap.set(key, value.replace(regex, myMap.get(regex.exec(value)[1])));  // Replace the value of the key
+                }
             }
-            
+        } while (mapIsPure == 0);
+
+        for (let [key, value] of myMap.entries()) {                                         // Final iteration to print the output map
+        
             console.log(key + ': ' + myMap.get(key));
         }
     });
